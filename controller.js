06bbc8631fd8dcc40c12792bdc93fa25b726aa9e
jbanety <jbanety@gmail.com>
2015-02-06 12:15:45 +0100

    Ajout de controller.js

---
+++ controller.js
@@ -0,0 +1,165 @@
+/*
+ * @package     etd-framework/js
+ *
+ * @version     1.0
+ * @copyright   Copyright (C) 2015 ETD Solutions, SARL Etudoo. Tous droits réservés.
+ * @license     Apache License Version 2.0 (https://raw.githubusercontent.com/etd-framework/js/master/LICENSE)
+ * @author      ETD Solutions http://etd-solutions.com
+ */
+
+define(["jquery", "vendor/etdsolutions/js/app", "vendor/etdsolutions/js/text"], function($, app, text) {
+
+    /*
+     * Méthodes privées
+     */
+
+    function btnCall(self, btnSelector, options, handler) {
+
+        $(btnSelector).on('click', function(e) {
+
+            e.preventDefault();
+
+            var $this = $(this),
+                $icon = $this.find('.fa');
+
+            // On change la valeur.
+            var newValue = 1 - $this.data('current');
+
+            // On crée l'objet de données.
+            var data = {};
+
+            $.each(options.props, function(i, str) {
+                data[str] =  $this.data(i);
+            });
+
+            data[options.valueName] = newValue;
+            data[options.token] = '1';
+
+            if (!$icon.hasClass('fa-spin')) {
+                $icon.removeClass('fa-warning fa-check fa-times').addClass('fa-spin fa-refresh');
+
+                self.task(options.uri, data, function(data) {
+
+                    if (options.successCallback) {
+                        options.successCallback(data, $this);
+                    }
+
+                    $this.data('current', newValue);
+
+                    var btnParams = handler(newValue, options.states || {});
+
+                    $this.tooltip('destroy');
+                    $this.attr('title', text._(btnParams.txt));
+                    $this.tooltip({
+                        container: 'body',
+                        html: true
+                    });
+                    $icon.removeClass('fa-spin fa-refresh').addClass(btnParams.icon);
+
+                }, function() {
+
+                    $icon.removeClass('fa-spin fa-refresh').addClass('fa-warning');
+
+                });
+            }
+        });
+
+    }
+
+    /*
+     * Objet public
+     */
+
+    return {
+
+        createChooseBtn: function(btnSelector, options) {
+
+            btnCall(this, btnSelector, options, function(newValue, states) {
+
+                var btnParams = {
+                    txt: states.default.txt,
+                    icon: states.default.icon
+                };
+
+                if (states[newValue]) {
+
+                    btnParams.txt = states[newValue].txt;
+                    btnParams.icon = states[newValue].icon;
+
+                }
+
+                return btnParams;
+
+            });
+
+            return this;
+
+        },
+
+        createToggleBtn: function(btnSelector, options) {
+
+            btnCall(this, btnSelector, options, function(newValue, states) {
+
+                var btnParams = {
+                    txt: '',
+                    icon: ''
+                };
+
+                if (newValue == 1) {
+                    btnParams.txt = 'APP_GLOBAL_YES';
+                    btnParams.icon = 'fa-check';
+                } else {
+                    btnParams.txt = 'APP_GLOBAL_NO';
+                    btnParams.icon = 'fa-times';
+                }
+
+                return btnParams;
+
+            });
+
+            return this;
+
+        },
+
+        /**
+         * Méthode pour appeler un controller
+         *
+         * @param uri
+         * @param data
+         * @param successCallback
+         * @param failCallback
+         * @returns jqXHR
+         */
+        task: function(uri, data, successCallback, failCallback) {
+
+            return $.ajax(uri, {
+                dataType: 'json',
+                type: 'POST',
+                data: data
+            }).done(function(data) {
+                if (data.error) {
+                    app.raiseError(data.message);
+                    if (failCallback) {
+                        failCallback(data);
+                    }
+                } else if(successCallback) {
+                    successCallback(data);
+                }
+            }).fail(function(obj) {
+                if (obj.responseJSON && obj.responseJSON.error) {
+                    app.raiseError(obj.responseJSON.message);
+                    if (failCallback) {
+                        failCallback(obj.responseJSON);
+                    }
+                } else {
+                    if (failCallback) {
+                        failCallback();
+                    }
+                }
+            });
+
+        }
+
+    };
+
+});
